299b093017cad8724998cbe10af65c96b070d643
Zburlea Stefan <zburleastefan@yahoo.com>
2023-05-11 14:18:47 +0300

    update no1

--- next.config.js
+++ next.config.js
@@ -26,26 +26,6 @@ const nextConfig = {
         dynamicImports: true,
         css: true
     }, 
-    webpack(config) {
-      config.module.rules.push({
-          test: /\.js$/,
-          use: {
-              loader: '@swc/loader',
-              options: {
-                  jsc: {
-                      parser: {
-                          syntax: 'ecmascript',
-                          jsx: true
-                      },
-                      transform: {
-                          react: true
-                      }
-                  }
-              }
-          }
-      });
-      return config;
-  }
 }
 
 module.exports = nextConfig
