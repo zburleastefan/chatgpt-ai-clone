48e658ac34791efefe15a7fb8b3541e159542ec1
Zburlea Stefan <zburleastefan@yahoo.com>
2023-05-31 08:33:36 +0300

    dark mode commit

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,12 @@ export default function Head(){
         <>
             <title>ChatGpt AI Clone</title>
             <meta content="width=device-width, initial-scale=1" name="viewport"/>
+             {/* <!-- Chrome, Firefox OS and Opera --> */}
+             <meta name="theme-color" content="#000000"/>
+            {/* <!-- Windows Phone --> */}
+            <meta name="msapplication-navbutton-color" content="#000000"/>
+            {/* <!-- iOS Safari --> */}
+            <meta name="apple-mobile-web-app-status-bar-style" content="#000000"/>
             <link rel="icon" href="/favicon.ico"/>
         </>
     )
